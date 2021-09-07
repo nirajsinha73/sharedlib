c6ca156f34b284f2bbd6930d57f5d171e9fba9aa
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-09-07 10:47:11 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -7,29 +7,10 @@ def call(String repoUrl) {
                steps {
                    sh "mvn --version"
                    sh "java -version"
-               }
-           }
-           stage("Checkout Code") {
-               steps {
-                   git branch: 'master',
-                       url: "${repoUrl}"
-               }
-           }
-           stage("Cleaning workspace") {
-               steps {
-                   sh "mvn clean"
-               }
-           }
-           stage("Running Testcase") {
-              steps {
-                   sh "mvn test"
-               }
-           }
-           stage("Packing Application") {
-               steps {
-                   sh "mvn package -DskipTests"
+                  echo " done"
                }
            }
        }
-   }
-}
+  }
+ }
+           
