e3b5181436c090c52ffc554337ff1b5d5152a4b2
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-09-09 12:44:12 +0530

    Update jenkinsLambdaTest.groovy

--- vars/jenkinsLambdaTest.groovy
+++ vars/jenkinsLambdaTest.groovy
@@ -2,10 +2,7 @@ def call(String repoUrl) {
    pipeline{
        agent any
     
-       environment {
-           LAMBDA_TEST_CRED = credentials('f66cb042-8b69-41fc-86ac-5a84102d06f5')
-       }
-       stages {
+        stages {
            stage("Tools initialization") {
                steps {
                   echo "pipeline started"
