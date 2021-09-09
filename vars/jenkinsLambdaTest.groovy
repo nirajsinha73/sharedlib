73217e0daf85e460adb8dc8e349d83b3a15c53b6
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-09-09 08:29:15 +0530

    Create jenkinsLambdaTest.groovy

---
+++ vars/jenkinsLambdaTest.groovy
@@ -0,0 +1,31 @@
+def call(String repoUrl) {
+   pipeline{
+       agent any
+       tools {
+           maven 'Maven 3.5.0'
+           jdk 'jdk8'
+       }
+       environment {
+           LAMBDA_TEST_CRED = credentials('lambdatest-site-cred')
+       }
+       stages {
+           stage("Tools initialization") {
+               steps {
+                       sh "mvn --version"
+                       sh "java -version"
+                   }
+               }
+           stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                   url: "${repoUrl}"
+               }
+           }
+           stage("Running Testcase") {
+               steps {
+                   sh "mvn -Dusername=${LAMBDA_TEST_CRED_USR} -DaccessKey=${LAMBDA_TEST_CRED_PSW} test"
+               }
+           }
+       }
+   }
+}
