18f36dfe85cd26e85200d38b433240fcff867d72
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-09-09 08:58:35 +0530

    Update jenkinsLambdaTest.groovy

--- vars/jenkinsLambdaTest.groovy
+++ vars/jenkinsLambdaTest.groovy
@@ -16,8 +16,8 @@ def call(String repoUrl) {
            stage("Checkout Code") {
                steps {
                   echo "Checkout Code started"
-                   git branch: 'main',
-                   url: "$https://github.com/nirajsinha73/sharedlib.git"
+                   #git branch: 'main',
+                 #  url: "$https://github.com/nirajsinha73/sharedlib.git"
                   echo "Checkout Code Completed"
                }
            }
