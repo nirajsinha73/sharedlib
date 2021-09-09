5daeac0705d3553751f007e569bde6d0394462f5
nirajsinha73 <54464397+nirajsinha73@users.noreply.github.com>
2021-09-09 08:17:18 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -2,8 +2,8 @@ def call(String repoUrl) {
   pipeline {
        agent any
        tools {
-           maven 'Maven 3.5.0'
-           jdk 'jdk8'
+           maven 'null'
+           jdk 'null'
        }
        stages {
            stage("Tools initialization") {
